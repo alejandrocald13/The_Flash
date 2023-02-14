e239f6ba672de533a1e6627bf4b2ed80257ae4d7
Alejandro Calderón <robertoalejandrocalderon@gmail.com>
2023-02-14 09:27:18 -0600

    SERIE VI Y SERIE V
    Se ejecutó el archivo main.py y se corrigió el archivo .gitignore para que ignorará la carpeta de Modulos como un cambio.

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ print('Mi nombre es Roberto Alejandro Calderón Martínez')
 print('Mi carné estudiantil es: 1557723')
 print('Resido en Quetzaltenango.')
 print('Mi universidad es la Universidad Rafael Landivar, Campus de Quetzaltenango.')
+
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
